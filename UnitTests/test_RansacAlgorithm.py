c06fef7d531180642c5be7eb3bc1f12dea1e179e
saurabh <saurabh_dasgupta@hotmail.com>
2020-12-24 18:28:58 +0000

    RANSAC - added unit test with 2 lines

--- UnitTests/test_RansacAlgorithm.py
+++ UnitTests/test_RansacAlgorithm.py
@@ -37,6 +37,22 @@ class Test_testRansac(unittest.TestCase):
         self.assertLessEqual(len(first_line.inliers), 11, 'Inliers should be correctly detected')
         pass
 
+    def test_WhenImage_Has_Two_ProminentLine_Then_1_LineShouldBe_Returned(self):
+        folder_script=os.path.dirname(__file__)
+        file_test_image=os.path.join(folder_script,"./data/","SampleWith2ProminentLine.png")
+        np_image=io.imread(file_test_image,as_gray=True)
+        width=np_image.shape[1]
+        height=np_image.shape[0]
+        points=Util.create_points_from_numpyimage(np_image)
+
+        algo=RansacAlgorithm(width,height,points)
+        results:List[RansacLineInfo]=algo.run()
+        self.assertEqual(len(results), 1, 'message')
+        first_line:RansacLineInfo=results[0]
+        self.assertGreaterEqual(len(first_line.inliers), 10, 'Inliers should be correctly detected')
+        self.assertLessEqual(len(first_line.inliers), 13, 'Inliers should be correctly detected')
+        pass
+
     # def test_WhenImage_Has_Two_Prominent_Lines_Then_2_LineShouldBe_Returned(self):
     #     folder_script=os.path.dirname(__file__)
     #     file_test_image=os.path.join(folder_script,"./data/","SampleWith2ProminentLine.png")
