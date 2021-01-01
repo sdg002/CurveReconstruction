b8d6057e229ce1cf16c633c5b12baf0e22a23e2b
saurabh <saurabh_dasgupta@hotmail.com>
2021-01-01 17:54:23 +0000

    Unit test with 2 distinct large lines (aggregator)

--- UnitTests/algorithm/test_PatchByPatchLineAggregator.py
+++ UnitTests/algorithm/test_PatchByPatchLineAggregator.py
@@ -118,9 +118,48 @@ class Test_PatchByPatchLineAggregator(unittest.TestCase):
                 self.assertGreaterEqual(angle,44,"The slope should be about 45 degrees")
                 self.assertLessEqual(angle,46,"The slope should be about 45 degrees")
             
-    @unittest.skip("To be done")
-    def test_Using_LargeFile_With_2ProminentLines_And_2X2_patches_Then_ShouldReturn_1_Cluster(self):
-        raise Exception("Not yet implemented")
+    def test_Using_LargeFile_With_2ProminentLines_And_2X2_patches_Then_ShouldReturn_2_Cluster(self):
+        """
+        We are testing on an image that has 2 distinct large lines
+        """
+        folder_script=os.path.dirname(__file__)
+        file_test_image=os.path.join(folder_script,"./data/PatchByPatchLineAggregator","SampleWith2ProminentLine.png")
+        algo = PatchByPatchRansac(file_test_image)
+        algo.Dimension=70
+        algo.ransac_threshold_distance=2
+        array_of_patches:np.ndarray=algo.run1()
+        self.assertEqual(2,array_of_patches.shape[0] , 'The shape of the array must be equal to the number of cells along Y axis')
+        self.assertEqual(2,array_of_patches.shape[1] , 'The shape of the array must be equal to the number of cells along Y axis')
+
+        filter_handler=PatchByPatchStatisticalFilter(array_of_patches)
+        filtered_array_of_patches=filter_handler.filter_using_median_distributionindex()
+
+        patch_aggregator=PatchByPatchLineAggregator(filtered_array_of_patches)
+        patch_aggregator.rho_threshold=algo.ransac_threshold_distance
+        patch_aggregator.theta_threshold= 10 * math.pi/180 #10 degrees
+        array_of_patches=patch_aggregator.find_connected_lines_in_adjacent_patches()
+        flat_array_of_patches=array_of_patches.flatten()
+
+        self.assertEqual(1, len(flat_array_of_patches), '1 resulting patches = (2-1)*(2-1)')
+        for clusters in flat_array_of_patches:
+            self.assertEqual(2, len(clusters), 'We expect 2 cluster of lines, i.e. a cluster with prominent large line and a cluster with the shorter line')
+            bigger_cluster=None
+            smaller_cluster=None
+            if len(clusters[0].ransac_lines[0].inliers) > len(clusters[1].ransac_lines[0].inliers):
+                bigger_cluster=clusters[0]
+                smaller_cluster=clusters[1]
+            else:
+                bigger_cluster=clusters[1]
+                smaller_cluster=clusters[0]
+
+            slope_of_bigger_cluster=math.atan(-bigger_cluster.ransac_lines[0].line.A/bigger_cluster.ransac_lines[0].line.B) * 180/math.pi
+            slope_of_smaller_cluster=math.atan(-smaller_cluster.ransac_lines[0].line.A/smaller_cluster.ransac_lines[0].line.B) * 180/math.pi
+
+            self.assertGreaterEqual(slope_of_bigger_cluster,38,"The slope should be within expected range")
+            self.assertLessEqual(slope_of_bigger_cluster,42,"The slope should be within expected range")
+
+            self.assertGreaterEqual(slope_of_smaller_cluster,-14,"The slope should be within expected range")
+            self.assertLessEqual(slope_of_smaller_cluster,-10,"The slope should be within expected range")
 
 
     def  test_find_connected_lines_in_across_all_patches_3x3patches_and_1Cluster(self):
@@ -209,13 +248,16 @@ class Test_PatchByPatchLineAggregator(unittest.TestCase):
         self.assertEqual(2, len(list_of_connected_lines), '2 cluster should be returned')
         self.assertEqual(2, len(list_of_connected_lines[0].ransac_lines), 'The cluster should contain all the ransac lines')
         self.assertEqual(2, len(list_of_connected_lines[1].ransac_lines), 'The cluster should contain all the ransac lines')
-        
+
         self.assertTrue(line0 in list_of_connected_lines[0].ransac_lines)
         self.assertTrue(line1 in list_of_connected_lines[0].ransac_lines)
 
         self.assertTrue(line2 in list_of_connected_lines[1].ransac_lines)
         self.assertTrue(line3 in list_of_connected_lines[1].ransac_lines)
 
+    @unittest.skip("To be done")
+    def test_ImageWith2Lines(self):
+        raise Exception("to be done")
 
 if __name__ == '__main__':
     unittest.main()
