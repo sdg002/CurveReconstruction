7c93666adf0e276ed6c09c64389fefeb53df2ded
saurabh <saurabh_dasgupta@hotmail.com>
2021-04-14 23:29:33 +0100

    adaptive ransac on parabola

--- Playground/RansacAdaptive.py
+++ Playground/RansacAdaptive.py
@@ -1,3 +1,23 @@
+'''
+What was I doing here?
+----------------------
+    Use mean/mode of the nearest neighbour distance to determine Ransac threshold
+    Then run RANSAC on sample images
+
+Where have I reached?
+---------------------
+    Tried on LineSamples
+    Tried on Parabola samples
+
+what did I learn from Parabola?
+-------------------------------
+    Worked generally well
+
+What did not work well?
+-----------------------
+    But, for the most crowded parabola, I could not get the best line even after 24,00,000 iterations
+
+'''
 from operator import mod
 from Common.Point import Point
 import numpy as np
@@ -9,10 +29,11 @@ import statistics
 from skimage.measure import LineModelND, ransac
 import datetime
 from scipy import stats
+import glob
 
-def run(inputfilename:str):
+def run(inputfilename:str,num_trials:int=1000):
     print("-------------------------------------------")
-    print("Processing input file %s" % (inputfilename))
+    print("Processing input file %s with num_trials=%d" % (inputfilename,num_trials))
     folder_script=os.path.dirname(__file__)
     file_noisy_curve=os.path.join(folder_script,"./in/",inputfilename)
     np_image=io.imread(file_noisy_curve,as_gray=True)
@@ -41,7 +62,7 @@ def run(inputfilename:str):
     distance_from_line=mode *.5
     min_samples=3
     model_robust, inliers = ransac(points_array, LineModelND, min_samples=min_samples,
-                                   residual_threshold=distance_from_line, max_trials=1000)
+                                   residual_threshold=distance_from_line, max_trials=num_trials)
     #
     #Generate points on the RANSAC line
     #
@@ -57,11 +78,12 @@ def run(inputfilename:str):
     filename_noextension=os.path.splitext(inputfilename)[0]
     now=datetime.datetime.now()
     count_of_inliers=len(list(map(lambda x: x==True, inliers)))
-    filename_result=("adaptive-%s-threshold-%d-minsamples-%d-inliers-%d.result.png") % (filename_noextension,distance_from_line,min_samples,count_of_inliers)
+    filename_result=("adaptive-%s-threshold-%d-minsamples-%d-inliers-%d-trials-%d.result.png") % (filename_noextension,distance_from_line,min_samples,count_of_inliers, num_trials)
     file_result=os.path.join(folder_script,"./out/",filename_result)
 
     np_superimposed=Util.superimpose_points_on_image(np_image,new_points,100,255,100)
     io.imsave(file_result,np_superimposed)
+    print("Output written to %s" % (file_result))
     pass
 
     pass
@@ -69,6 +91,30 @@ def run(inputfilename:str):
 #
 #
 #
-run("LineSample1.png")
-run("LineSample2.png")
-run("LineSample3.png")
+def run_selectedfiles():
+    run("LineSample1.png")
+    run("LineSample2.png")
+    run("LineSample3.png")
+
+def run_selected_filepattern(pattern:str,num_trials:int):
+    folder_script=os.path.dirname(__file__)
+    folder_with_files=os.path.join(folder_script,"./in/")
+    matching_files=glob.glob(folder_with_files+pattern)
+    for file in matching_files:
+        filename=os.path.basename(file)
+        run(filename,num_trials=num_trials)
+#run_selectedfiles()
+#run_selected_filepattern("*parabola*.png")
+run_selected_filepattern("*parabola.W=500.H=200.MAXD=10.SP=0.80.21.png*",num_trials=2400000)
+#parabola.W=500.H=200.MAXD=10.SP=0.80.21.png.21-threshold-1-minsamples-3-inliers-1093.result
+#parabola.W=500.H=200.MAXD=10.SP=0.80.21.png.21-threshold-1-minsamples-3-inliers-1093.result
+
+#
+#You were here
+'''
+You did an adaptive determination of threshold using the mode metric of the NN distance. 
+This worked well with 3 samples
+Bring in more samples
+Do a patch by patch on a larger sine curve
+'''
+#
